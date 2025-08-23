254f33606dbb37c0d826c2964c1b7cfed33e7574
Nitin Kumar <nitinrishab2000@gmail.com>
2025-08-24 02:47:13 +0530

    Footer Section added

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles.css";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <p className="footer-text">
+        © {currentYear} Moviedux, All rights reserved
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
