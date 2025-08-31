6902244385d7f325928f7f02d496eb72aa264af4
Nitin Kumar <nitinrishab2000@gmail.com>
2025-09-01 02:56:04 +0530

    Props Completed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="div">
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer></Footer>
     </div>
---
+++ src/components/Watchlist.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles.css";
+
+function Watchlist() {
+  return (
+    <div>
+      <h1 className="title">Your Watchlist</h1>
+    </div>
+  );
+}
+
+export default Watchlist;
